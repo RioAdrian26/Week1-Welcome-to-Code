a29da92f3d0063afb5ccec24685a1aecefefc891
RioAdRian26 <muhammadrioadrian123@gmail.com>
2025-05-21 19:35:58 +0700

    soal1

---
+++ quiz/part1/soal1.js
@@ -0,0 +1,18 @@
+let nama = "";
+let peran = "";
+
+if (nama == ""){
+    console.log("nama wajib diisi")
+}else if (peran == ""){
+    console.log("pilih peranmu memulai game")
+}
+
+if (peran == "ksatria"){
+    console.log(`halo ksatria ${nama} , kamu dapat menyerang dengan senjatamu!`)
+}else if (peran == "tabib"){
+    console.log(`halo tabib ${nama} , kamu akan membantu temanmu yang terluka`)
+}else if (peran == "penyihir"){
+    console.log(`halo penyihir ${nama} , ciptakan keajaiban yang membantu kemenanganmu!`)
+}else{
+    console.log("kamu jadi bot aja ya")
+}
