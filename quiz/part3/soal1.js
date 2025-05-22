d6247ebcfe6c49d733506007323d4cd976a870a4
RioAdRian26 <muhammadrioadrian123@gmail.com>
2025-05-22 17:16:01 +0700

    soal1

--- quiz/part3/soal1.js
+++ quiz/part3/soal1.js
@@ -1,11 +1,11 @@
 let i = 0;
 let j = 5;
-console.log("Pertama");
-while (i < j) {
+console.log("Pertama:");
+while (i <= j) {
   console.log(i);
   i++;
 }
-console.log("Kedua");
+console.log("Kedua:");
 while (j >= 0) {
   console.log(j);
   j--;
---
+++ quiz/part3/soal2.js
@@ -0,0 +1,4 @@
+let i = 0;
+let j = 5;
+
+for (let i=0)
