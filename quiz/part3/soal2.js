7690320057cfc82840f59de61f0de52497f321e1
RioAdRian26 <muhammadrioadrian123@gmail.com>
2025-05-22 17:24:55 +0700

    soal2

--- quiz/part3/soal2.js
+++ quiz/part3/soal2.js
@@ -1,4 +1,11 @@
 let i = 0;
 let j = 5;
 
-for (let i=0)
+console.log("Pertama");
+for (let i = 0; i <= j; i++) {
+  console.log(i);
+}
+console.log("Kedua");
+for (let j = 5; j >= 0; j--) {
+  console.log(j);
+}
