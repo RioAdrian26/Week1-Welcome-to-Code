970d4ab6526bab1b994672abccdad02280515b34
RioAdRian26 <muhammadrioadrian123@gmail.com>
2025-05-22 12:17:17 +0700

    soal3

---
+++ quiz/part2/soal3.js
@@ -0,0 +1,12 @@
+let word3 = "wow JavaScript is so cool";
+let exampleFirstWord3 = word3.substring(0, 3);
+let exampleTwotWord = word3.substring(4, 14);
+let exampleThirdWord = word3.substring(15, 17);
+let exampleFourWord = word3.substring(18, 20);
+let exampleFiveWord = word3.substring(21, 25);
+
+console.log("First Word: " + exampleFirstWord3);
+console.log("Two Word: " + exampleTwotWord);
+console.log("Third Word: " + exampleThirdWord);
+console.log("Four Word: " + exampleFourWord);
+console.log("Five Word: " + exampleFiveWord);
